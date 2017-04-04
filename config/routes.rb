3cb88fcc0ac6c798d293488095128b60da6e715e
Mike Schutte <tmikeschutte@gmail.com>
2017-04-04 12:48:17 -0600

    Add functionality for user login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: "links#index"
   
-  get '/login', to: "sessions#new"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   
   resources :users, only: [:new, :create]
 
