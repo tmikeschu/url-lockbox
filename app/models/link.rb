9cf12dc738475a6287a160f23e52b1b6c518f6bc
Mike Schutte <tmikeschutte@gmail.com>
2017-04-17 12:56:41 -0600

    Add logic and styling for read links
    * Toggle opacity from 1 to 0.5
    * Add tests to verify class toggling
    * Fix API request spec to include current user
    * Add boolean validation for Link#read
    * Change controller .find to .find_by_id

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,6 +2,7 @@ class Link < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of(:url, :title)
+  validates :read, inclusion: { in: [true, false] }
 
   scope :by_updated_at, -> { order("links.updated_at DESC") }
 end
--- spec/requests/api/v1/links_spec.rb
+++ spec/requests/api/v1/links_spec.rb
@@ -1,6 +1,12 @@
 require "rails_helper"
 
 RSpec.describe "Links API", type: :request do
+  let(:user) { create(:user) }
+
+  before do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  end
+
   describe "POST /api/v1/links" do
     it "creates a link in the database" do
       links_before = Link.count
@@ -19,7 +25,7 @@ RSpec.describe "Links API", type: :request do
       expect(response).to be_success
 
       link_card = response.body
-      expect(link_card).to include "<article id=\"link#{Link.last.id}\">"
+      expect(link_card).to include "<article id=\"link#{Link.last.id}\" class=\"\">"
       expect(link_card).to include "<h4>#{Link.last.title}</h4>"
       expect(link_card).to include Link.last.url
     end
@@ -32,6 +38,38 @@ RSpec.describe "Links API", type: :request do
 
       errors = JSON.parse(response.body)
       expect(errors).to match ["Url can't be blank", "Title can't be blank"]
+    end
+  end
+
+  describe "PATCH /api/v1/links/:id" do
+    let(:link) { create(:link, user: user) }
+
+    it "updates the read status" do
+      status_before = link.read
+
+      link_data = { link: { read: !link.read } }
+      patch "/api/v1/links/#{link.id}", params: link_data
+      expect(response).to be_success
+
+      link.reload
+      expect(link.read).to_not eq status_before
+    end
+
+    it "and returns the new object" do
+      link_data = { link: { read: !link.read } }
+      patch "/api/v1/links/#{link.id}", params: link_data
+
+      response_link = JSON.parse(response.body, symbolize_names: true)
+      updated_link = JSON.parse(link.reload.to_json)
+      
+      expect(updated_link.values).to match_array response_link.values
+    end
+
+    it "returns a 500 if data is bad" do
+      link_data = { link: { url: "" } }
+      patch "/api/v1/links/#{link.id}", params: link_data
+
+      expect(response.status).to eq 500
     end
   end
 end
