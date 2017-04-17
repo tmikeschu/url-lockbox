8c8f576343d06853c19a1d11ed03662c4375755d
Mike Schutte <tmikeschutte@gmail.com>
2017-04-17 11:08:03 -0600

    Implement marking links read/unread ajax
    * Add tests
    * Add javascript event handlers
    * Update view for jQuery friendliness

--- app/assets/javascripts/links.js
+++ app/assets/javascripts/links.js
@@ -5,6 +5,8 @@ $(document).ready(() => {
 class LinksManager {
   constructor() {
     $("input[value='Lock It Up']").on("click", this.lockItUp.bind(this))
+    $("body").on("click", "input[value='Mark as Read']", this.markAsRead.bind(this))
+    $("body").on("click", "input[value='Mark as Unread']", this.markAsUnread.bind(this))
   }
 
   lockItUp(event) {
@@ -31,12 +33,52 @@ class LinksManager {
     .fail(error => console.log(error))
   }
 
-   handleErrors() {
-     $(".create-link p").text("")
-     const urlMsg = $("#link_url")[0].validationMessage
-     const titleMsg = $("#link_title")[0].validationMessage
+  handleErrors() {
+    $(".create-link p").text("")
+    const urlMsg = $("#link_url")[0].validationMessage
+    const titleMsg = $("#link_title")[0].validationMessage
 
-     urlMsg && $("#link_url").after(`<p class="flash danger">${urlMsg}</p>`)
-     titleMsg && $("#link_title").after(`<p class="flash danger">${titleMsg}</p>`)
-   }
+    urlMsg && $("#link_url").after(`<p class="flash danger">${urlMsg}</p>`)
+    titleMsg && $("#link_title").after(`<p class="flash danger">${titleMsg}</p>`)
+  }
+  
+  markAsUnread(event) {
+    event.preventDefault();
+
+    const linkId = $(event.target).siblings("#link_id").val()
+    const link = { link: { read: false } }
+
+    this.updateReadStatus(link, linkId)
+  }
+
+  markAsRead(event) {
+    event.preventDefault();
+
+    const linkId = $(event.target).siblings("#link_id").val()
+    const link = { link: { read: true } }
+
+    this.updateReadStatus(link, linkId)
+  }
+
+  updateReadStatus(link, linkId) {
+    $.ajax({
+      type: "PATCH",
+      url: "/api/v1/links/" + linkId,
+      data: link,
+    })
+    .done(this.updateLinkStatus)
+    .fail(this.displayFailure);
+  }
+
+  updateLinkStatus(link) {
+    const buttonText = link.read ? "Mark as Unread" : "Mark as Read"
+    $(`#link${link.id}`).find('p').text(`Read? ${link.read}`)
+    $(`#link${link.id}`).find('.read-unread').val(buttonText)
+  }
+
+  displayFailure(failureData){
+    console.log("FAILED attempt to update Link: " + failureData.responseText);
+  }
 }
+
+
